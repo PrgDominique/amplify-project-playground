f4e2e015c06aabe37a9bad3d872eb1fb59f11acc
PrgDominique <dominiquesimbillo10@gmail.com>
2023-03-21 13:23:39 +0800

    add switch statement for types

--- amplify/backend/function/amplifyprojectplaygrfaa54b72/src/index.js
+++ amplify/backend/function/amplifyprojectplaygrfaa54b72/src/index.js
@@ -1,6 +1,8 @@
 exports.handler = async event => {
-  const response = await fetch('https://fakestoreapi.com/products/1');
-  const product = await response.json();
-
-  return product;
 };
+
+// const response = await fetch('https://fakestoreapi.com/products/1');
+//   const product = await response.json();
+
+// console.log(`EVENT: ${JSON.stringify(event)}`);
+//   return product;
