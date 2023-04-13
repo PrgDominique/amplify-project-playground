4a239c873d0914a0454cb4f55e6686cc905ab520
PrgDominique <dominiquesimbillo10@gmail.com>
2023-04-13 13:25:26 +0800

    testing some api

--- amplify/backend/function/amplifyprojectplaygrfaa54b72/__tests__/index.test.js
+++ amplify/backend/function/amplifyprojectplaygrfaa54b72/__tests__/index.test.js
@@ -1,17 +1,24 @@
-const {describe, it} = require('@jest/globals')
+const { describe, it } = require('@jest/globals');
+const { handler } = require('../src');
 
+let result
 
-describe ('this would return a value', () => {
-    it('return the args', () => {
-        const myMock1 = jest.fn();
-        const args1 = 'first arg';
-        const args2 = 'second arg';
-       myMock1(args1, args2);
-        // console.log(myMock1.mock.instances);
-        // const a = new myMock1();
-        // a.test = 'test';
-        // console.log(myMock1.mock.instances);
-        expect(myMock1.mock.calls[0][0]).toBe('first arg');
-        expect(myMock1.mock.calls[0][1]).toBe('second arg');
-    })
-})
+describe('this would return a value', () => {
+  it('return the args', () => {
+    const myMock1 = jest.fn();
+    const args1 = 'first arg';
+    const args2 = 'second arg';
+    myMock1(args1, args2);
+    // console.log(myMock1.mock.instances);
+    // const a = new myMock1();
+    // a.test = 'test';
+    // console.log(myMock1.mock.instances);
+    expect(myMock1.mock.calls[0][0]).toBe('first arg');
+    expect(myMock1.mock.calls[0][1]).toBe('second arg');
+  });
+
+  it('return an id', async () => {
+     result = await handler(3);
+    console.log('result: ' + JSON.stringify({ result }, null, 2));
+  });
+});
--- amplify/backend/function/amplifyprojectplaygrfaa54b72/src/index.js
+++ amplify/backend/function/amplifyprojectplaygrfaa54b72/src/index.js
@@ -1,8 +1,8 @@
 exports.handler = async event => {
+  const response = await fetch(`https://fakestoreapi.com/products/${event}`);
+  const product = await response.json();
+  console.log(product);
+  return product
 };
 
-// const response = await fetch('https://fakestoreapi.com/products/1');
-//   const product = await response.json();
 
-// console.log(`EVENT: ${JSON.stringify(event)}`);
-//   return product;
